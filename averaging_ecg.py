fec08def32e65cd8b4f924fb26b19b95a5758e9f
souvlasvegas <a.miltiadous@uoi.gr>
2022-05-10 10:19:52 +0300

    Update averaging_ecg.py

--- averaging_ecg.py
+++ averaging_ecg.py
@@ -13,10 +13,11 @@ import os
 from pathlib import Path
 import numpy as np
 
-files = filedialog.askopenfilenames(title="Select all csv's")
 
-column_names = ["number", "average_bpm"]
-new_df=pd.DataFrame(columns=column_names)
+#choose eyes_closed_files
+files = filedialog.askopenfilenames(title="Select all csv's with eyes_closed")
+column_names = ["number", "eyes_closed_bpm"]
+eyes_closed_df=pd.DataFrame(columns=column_names)
 
 
 for file in files:
@@ -24,10 +25,38 @@ for file in files:
     number= filename.split(")")[0]
     file_df=pd.read_csv(file)
     average=file_df["bpm"].mean()
-    thisdict = {"number": number,"average_bpm": average}
-    new_df=new_df.append(thisdict,ignore_index=True)
+    thisdict = {"number": number,"eyes_closed_bpm": average}
+    eyes_closed_df=eyes_closed_df.append(thisdict,ignore_index=True)
     
+#choose eyes_open_calm files
+files = filedialog.askopenfilenames(title="Select all csv's with eyes_open")
+column_names = ["number", "eyes_open_bpm"]
+eyes_open_df=pd.DataFrame(columns=column_names)
 
+for file in files:
+    filename = Path(file).name
+    number= filename.split(")")[0]
+    file_df=pd.read_csv(file)
+    average=file_df["bpm"].mean()
+    thisdict = {"number": number,"eyes_open_bpm": average}
+    eyes_open_df=eyes_open_df.append(thisdict,ignore_index=True)
     
+#choose stressed    
+files = filedialog.askopenfilenames(title="Select all csv's with stress")  
+column_names = ["number", "stressed_bpm"]
+stressed_df=pd.DataFrame(columns=column_names)
+  
+for file in files:
+    filename = Path(file).name
+    number= filename.split(")")[0]
+    file_df=pd.read_csv(file)
+    average=file_df["bpm"].mean()
+    thisdict = {"number": number,"stressed_bpm": average}
+    stressed_df=stressed_df.append(thisdict,ignore_index=True)
     
-    
+newdf=pd.merge(eyes_closed_df,eyes_open_df,how='inner',on='number')
+newdf=pd.merge(newdf,stressed_df,how='inner',on='number')
+
+dirr=filedialog.askdirectory(title="where to save")
+
+newdf.to_csv(dirr+"/bpm_total.csv",index=False)
